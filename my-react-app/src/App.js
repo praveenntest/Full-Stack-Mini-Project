8aeb642f293b2b0d6dac2622ea881121a0ba45ac
praveentestvagrant <praveenn@testvagrant.com>
2023-12-04 14:29:32 +0530

    The Database connection and other code changes

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,9 +1,11 @@
 import { Form } from 'react-router-dom';
 import './App.css';
-import {useState} from 'react';
+import {useState,useEffect} from 'react';
 
 function App() {
 const [form, setForm] = useState({});
+const [users, setUsers] = useState([]);
+
 const handleForm = (e) => {
 console.log(e.target.name, e.target.value)
 setForm({
@@ -15,17 +17,31 @@ setForm({
 const handleSubmit = async (e) => {
 e.preventDefault();
 const response = await fetch('http://localhost:8080/demo',{
-method : 'GET'
-})
-
-const data = await response.text();
-console.log(response)  
+method : 'POST',
+body:JSON.stringify(form),
+headers:{
+  'Content-Type':'application/json'
 }
+})
+const data = await response.json();
+console.log(data)  
+}
+
+const getUsers = async () => {
+  const response = await fetch('http://localhost:8080/demo',{
+  method : 'GET'
+})
+const data = await response.json();
+setUsers(data);
+}
+
+useEffect(() => {
+  getUsers()
+})
 
 return (
   <div>
-    <form  onSubmit={handleSubmit}>
-    
+    <form  onSubmit={handleSubmit}> 
     <p>{JSON.stringify(form)}</p>
     <span>username</span>
     <input type="text" name='username' onChange={handleForm}></input>
@@ -33,6 +49,11 @@ return (
     <input type="text" name='password' onChange={handleForm}></input>
     <input type="submit"></input>
     </form>
+      <div>
+        <ul>
+          {users.map(user=><li key={user._id}>{user.username},{user.password}</li>)}
+        </ul>
+      </div>
   </div>
   );
 }
